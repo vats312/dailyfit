ce63bb86b8c35507a8f0d25d9e9ae46f80cdc0f4
rajkaria21 <rajkaria72@gmail.com>
2020-03-12 22:32:44 +0530

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { RegisterComponent } from './register/register.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
-import { PhotodetailComponent } from './photodetail/photodetail.component';
 import { HealthdetailsComponent } from './register/healthdetails/healthdetails.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
@@ -12,7 +11,6 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 const routes: Routes = [
   {path:'', component:HeaderComponent},
   
-  {path:'photodetail',component:PhotodetailComponent},
   {path:'register', component:RegisterComponent},
   {path:'about',component:AboutComponent},
   {path:'contact',component:ContactComponent},
